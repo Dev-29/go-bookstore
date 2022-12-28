3f745c6d3074c87da6ddd70283c4967b82c423be
Dev-29 <devabhiram@gmail.com>
2022-12-28 20:29:18 +0530

    added local.env file

--- go.mod
+++ go.mod
@@ -3,10 +3,14 @@ module github.com/Dev-29/go-bookstore
 go 1.19
 
 require (
-	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.0
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
 	gorm.io/driver/mysql v1.4.4
 	gorm.io/gorm v1.24.2
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 )
--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,20 +1,22 @@
 package config
 
 import (
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-const DB_USERNAME = "root"
-const DB_PASSWORD = "Devabhiram@2003"
-const DB_NAME = "go_db"
-const DB_HOST = "localhost"
-const DB_PORT = "3306"
-
 var db *gorm.DB
 
 func Connect() {
-	d, err := gorm.Open(mysql.Open(DB_USERNAME+":"+DB_PASSWORD+"@tcp("+DB_HOST+":"+DB_PORT+")/"+DB_NAME+"?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{})
+	e := godotenv.Load("local.env")
+	if e != nil {
+		panic(e)
+	}
+	URL := os.Getenv("DB_URL")
+	d, err := gorm.Open(mysql.Open(URL), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
